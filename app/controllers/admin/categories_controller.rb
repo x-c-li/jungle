752932c3920dd44675b4b542de3170f147d8adb0
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-20 13:14:36 -0400

    create controller class

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,32 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: 'Jungle', password: 'book'
+
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :category_id
+    )
+  end
+
+end
