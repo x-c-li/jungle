573a9af3919ab5b6a122cad3aaa71118a752de3f
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-20 16:06:11 -0400

    fix typo

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -11,7 +11,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(product_params)
+    @category = Category.new(category_params)
 
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
