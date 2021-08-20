09460d8684b6ef500634098eb6e19fddb5575e57
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-20 19:14:34 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210820211601) do
+ActiveRecord::Schema.define(version: 20210820225643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20210820211601) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
 
   add_foreign_key "line_items", "orders"
