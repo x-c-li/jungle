cf3cbb25295ee379dd7acfdfd1b2e8e523a1387a
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-20 17:23:06 -0400

    create migration to create user table

---
+++ db/migrate/20210820211601_create_user_table.rb
@@ -0,0 +1,10 @@
+class CreateUserTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name 
+      t.string :last_name
+      t.string :email 
+      t.timestamps null: false
+    end
+  end
+end
