89d7cfbe21e64aa6b7b8e3ae572766eb84e9d75c
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-20 18:11:45 -0400

    create controller and specify routes and params, add html to index page

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,4 +1,30 @@
 class RegisterController < ApplicationController
   def index
+    @products = User.order(id: :desc).all
+  end
+
+  def new
+    @product = User.new
+  end
+  
+  def create
+    @user = User.new(user_params) #creates a new user model
+    #user params: all the data from the register form are stored here
+
+    if @user.save
+      redirect_to [:login], notice: 'User created!'
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email
+    )
   end
 end
