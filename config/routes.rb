37b38e2199bfe19030980d7dc3a799afd4ef2ae4
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-21 12:37:10 -0400

    add routes for register and logout

--- config/routes.rb
+++ config/routes.rb
@@ -20,16 +20,10 @@ Rails.application.routes.draw do
   
   resources :about
 
-  resources :login
-  resources :register
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
-end
-GifVault::Application.routes.draw.do 
-  # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-  root to: 'gif#cool'
-
-  # These routes will be for signup. The first renders a form in the browse, the second will 
-  # receive the form and create a user in our database using the data given to us by the user.
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
