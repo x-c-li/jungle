130aea53e0b3a445e69abfef5808ce98777c3ff3
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-21 13:01:23 -0400

    replace signup with register

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  get '/signup' => 'users#new'
+  get '/register' => 'users#new'
   post '/users' => 'users#create'
 
 end
