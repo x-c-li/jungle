562572090e9767f5db7ce0ad86d0cf8d8124418d
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-23 17:27:50 -0400

    write tests to check inverse of what you want: want invalid if field not filled/nil

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,21 +15,25 @@ RSpec.describe Product, type: :model do
       @category = Category.new(name: 'Book')
       @product = Product.new(name: nil, price: 123, quantity: 111, category: @category)
       expect(@product).to_not be_valid
-      puts 'ERROR MESSAGE', @product.errors.full_messages
+      # puts 'ERROR MESSAGE', @product.errors.full_messages
     end 
 
-    it 'returns false if price is not there' do
-      expect().to be true 
-    end 
-   
-    it 'returns false if quantity is not there' do 
-      expect().to be true 
-
+    it 'shows not valid if product price is nil' do
+      @category = Category.new(name: 'Book')
+      @product = Product.new(name: 'name', quantity: 111, category: @category)
+      expect(@product).to_not be_valid
     end 
    
-    it 'returns false if category is not there' do 
-      expect().to be true 
-
+    it 'shows not valid if product quantity is nil' do 
+      @category = Category.new(name: 'Book')
+      @product = Product.new(name: 'name', price: 123, quantity: nil, category: @category)
+      expect(@product).to_not be_valid
+    end 
+   
+    it 'shows not valid if product category is nil' do 
+      @category = Category.new(name: 'Book')
+      @product = Product.new(name: 'name', price: 123, quantity: 111, category: nil)
+      expect(@product).to_not be_valid
     end 
 
   end 
