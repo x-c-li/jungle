ea54fd1c85160460e2aa9efd86dfff72fa40e671
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-23 17:56:48 -0400

    add intial tests to check for necessary fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,20 +8,32 @@ require 'rails_helper'
 # validates :email, presence: true
 # validates :password_digest, presence: true
 
-RSpec.describe UserSpec, type: :model do
-  it 'returns not valid when user is missing first_name' do 
-  end 
-  it 'returns not valid when user does not have a last name' do 
-  end 
-  it 'returns not valid when user does not have an email' do 
-  end 
-  it 'returns not valid when user does not have a password' do 
+RSpec.describe User, type: :model do
+  
+  describe 'Validating Mandatory User Fields' do 
+    it 'returns not valid when user is missing first_name' do
+      @user = User.new(first_name: nil, last_name: 'doe', email:   'email@email.ca', password_digest: 'password') 
+      expect(@user).to_not be_valid
+    end 
+
+    it 'returns not valid when user does not have a last name' do 
+      @user = User.new(first_name: 'john', last_name: nil, email:   'email@email.ca', password_digest: 'password') 
+      expect(@user).to_not be_valid
+    end 
+    it 'returns not valid when user does not have an email' do 
+      @user = User.new(first_name: 'john', last_name: 'doe', email: nil, password_digest: 'password') 
+      expect(@user).to_not be_valid
+    end 
+    it 'returns not valid when user does not have a password' do 
+      @user = User.new(first_name: 'john', last_name: 'doe', email:   'email@email.ca', password_digest: nil) 
+      expect(@user).to_not be_valid
+    end 
   end 
 
   it 'returns not valid when password and password_confirmation do not match' do 
     @user = User.new(first_name: 'john', last_name: 'doe', email: 'email@email.ca', password_digest: 'password')
     @password_confirmation = 'pAssword'
-    expect
+    expect().to_not be_valid 
   end 
 
 end
